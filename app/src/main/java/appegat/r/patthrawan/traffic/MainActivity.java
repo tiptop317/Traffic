6c8440e6752e4a64aee2241c3c90a0181c644b1b
unknown <patthrawan@gmail.com>
2015-08-25 15:14:35 +0700

    Button Controller Finish

--- app/src/main/java/appegat/r/patthrawan/traffic/MainActivity.java
+++ app/src/main/java/appegat/r/patthrawan/traffic/MainActivity.java
@@ -1,17 +1,56 @@
 package appegat.r.patthrawan.traffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
     }
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.phonton2);
+                buttonMediaPlayer.start();
+
+                //Intent to Web
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("http://www.egat.co.th/index.php?option=com_content&view=article&id=149&Itemid=186"));
+                startActivity(objIntent);
+            }   //event
+        });
+
+    }
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
