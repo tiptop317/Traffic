1f07fca843253a868397a2f6b103fc480ddb9cf1
unknown <patthrawan@gmail.com>
2015-08-26 14:31:20 +0700

    Traffic Finish

--- app/src/main/java/appegat/r/patthrawan/traffic/DetailActivity.java
+++ app/src/main/java/appegat/r/patthrawan/traffic/DetailActivity.java
@@ -1,17 +1,77 @@
 package appegat.r.patthrawan.traffic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    //Explicit
+    TextView showTitleTextView, showDetailTextView;
+    ImageView showImageView;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //BindWidget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+        //Show Title
+        showTitle();
+
+        //Show Image
+        showImage();
+
+        //Show Detail
+        showDetail();
+
+    }   //OnCreate
+
+    private void showDetail() {
+        String strDetail[] = getResources().getStringArray(R.array.detail);
+        int intIndex = getIntent().getIntExtra("Index", 0);
+        showDetailTextView.setText(strDetail[intIndex]);
     }
+
+    private void showImage() {
+        int intImage = getIntent().getIntExtra("Image", R.drawable.traffic_01);
+        showImageView.setImageResource(intImage);
+    }
+
+    private void showTitle() {
+        String strTitle = getIntent().getStringExtra("Title");
+        showTitleTextView.setText(strTitle);
+    }
+
+    private void buttonController() {
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                //Intent obIntent = new Intent(DetailActivity.this, MainActivity.class);
+                //startActivity(obIntent);
+            }
+        });
+    }
+
+    private void bindWidget() {
+        showTitleTextView = (TextView) findViewById(R.id.txtShowTitle);
+        showDetailTextView = (TextView) findViewById(R.id.txtShowDetail);
+        showImageView = (ImageView) findViewById(R.id.imvShowImage);
+        backButton = (Button) findViewById(R.id.btnBack);
+    }   //bindWidget
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +94,4 @@ public class DetailActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   //Main Class
--- app/src/main/java/appegat/r/patthrawan/traffic/MainActivity.java
+++ app/src/main/java/appegat/r/patthrawan/traffic/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -34,14 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void createListView() {
         //Setup intDrawable
-        int intDrawable[] = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03, R.drawable.traffic_04,
+        final int intDrawable[] = {R.drawable.traffic_01, R.drawable.traffic_02, R.drawable.traffic_03, R.drawable.traffic_04,
                 R.drawable.traffic_05, R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
                 R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11, R.drawable.traffic_12,
                 R.drawable.traffic_13, R.drawable.traffic_14, R.drawable.traffic_15, R.drawable.traffic_16,
                 R.drawable.traffic_17, R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
 
         //Setup Title
-        String strTitle[] = new String[20];
+        final String strTitle[] = new String[20];
         strTitle[0] = "ห้ามเลี้ยวซ้าย";
         strTitle[1] = "ห้ามเลี้ยวขวา";
         strTitle[2] = "ตรงไป";
@@ -65,6 +66,19 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intDrawable, strTitle);
         trafficListView.setAdapter(objMyAdapter);
+
+        //Active When Click
+        trafficListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Intent to DetailActivity
+                Intent objIntent = new Intent(MainActivity.this, DetailActivity.class);
+                objIntent.putExtra("Title", strTitle[i]);
+                objIntent.putExtra("Image", intDrawable[i]);
+                objIntent.putExtra("Index", i);
+                startActivity(objIntent);
+            }   // event
+        });
 
     }   //createListView
 
